a4dd5318551b63715fab6e68d737c0537c4fb45a
Raxmatilla <isroilovrahmatilla9@gmail.com>
2025-01-07 16:08:27 +0500

    first commit

---
+++ config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/', include('apps.about.urls')),
+    path('', include('apps.base.urls')),
+    path('blog/', include('apps.blog.urls')),
+    path('contact/', include('apps.contact.urls')),
+    path('package/', include('apps.package.urls')),
+    path('services/', include('apps.service.urls')),
+] + debug_toolbar_urls()
